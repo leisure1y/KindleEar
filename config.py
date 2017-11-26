0d0b7a834b821126d92213337fa00e1e921367c4
leisure1y <leisure1y@hotmail.com>
2017-11-26 21:24:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ KindleEar配置文件，请务必修改开始两个配置（如果使用uploader
 """
 
 SRC_EMAIL = "moonmirros@gmail.com"  #Your gmail account for sending mail to Kindle
-DOMAIN = "https://imoonmirro.appspot.com" #Your domain of app
+DOMAIN = "https://moonmirros.appspot.com" #Your domain of app
 
 TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
 
